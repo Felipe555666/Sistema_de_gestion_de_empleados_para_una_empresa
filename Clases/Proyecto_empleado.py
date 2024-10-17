fad0055c47215e76ea7cce05ccaf8ca8e29e7f67
Felipe Aguero <felipe.aguero08@inacapmail.cl>
2024-10-17 00:14:52 -0300

    comienzo de herencias, faltan arreglos

--- Clases/Proyecto_empleado.py
+++ Clases/Proyecto_empleado.py
@@ -1,5 +1,5 @@
 class proyectoEmpleado:
     def __init__(self,Id,Id_proyecto,Id_empleado):
+        super().__init__(self, Id_proyecto)
         self.__Id = Id
         self.__Id_proyecto = Id_proyecto
-        self.__Id_empleado = Id_empleado

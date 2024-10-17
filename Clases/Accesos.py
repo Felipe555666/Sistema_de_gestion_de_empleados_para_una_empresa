d3e6d8e9e856c84cf546fff14b5eca1fbc2fc4d0
Felipe Aguero <felipe.aguero08@inacapmail.cl>
2024-10-16 23:49:19 -0300

    cambioxxxxx

--- Clases/Accesos.py
+++ Clases/Accesos.py
@@ -2,5 +2,5 @@ class Accesos:
     def __init__(self,Id,Id_modulo,Id_empleado):
         self.__Id = Id
         self.__Id_modulo = Id_modulo
-        self.__Id_empleado = Id_empleado
+        self.__Id_empleado = Id_empleado00
         

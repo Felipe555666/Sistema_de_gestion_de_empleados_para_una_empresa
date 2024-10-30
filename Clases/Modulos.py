893a919672893ae2a8ed264173f6782df7d1b4f4
Conito444 <conito.bravo2004@gmail.com>
2024-10-30 17:36:54 -0300

    Update Modulos.py

--- Clases/Modulos.py
+++ Clases/Modulos.py
@@ -3,3 +3,9 @@ class modulos:
         self._Id_modulo = Id_modulo
         self._Nombre = Nombre
         self.Nivel_requerido = Nivel_requerido
+        
+    def get_nivel_requerido(self):
+        return self.Nivel_requerido
+    
+    def validar_nivel(self, nivel_usuario):
+        return nivel_usuario >= self.Nivel_requerido

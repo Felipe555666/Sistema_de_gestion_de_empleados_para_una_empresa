c7b9d57af689f2440a8c210c1726109f6047bc86
Conito444 <conito.bravo2004@gmail.com>
2024-10-30 17:37:42 -0300

    Update Departamento.py

--- Clases/Departamento.py
+++ Clases/Departamento.py
@@ -6,3 +6,18 @@ class departamento(empleado):
         self._Id_departamento = Id_departamento
         self._Nombre = Nombre
         self._Telefono = Telefono
+        
+    def agregar_empleado(self, empleado):
+        if empleado not in self._empleados:
+            self._empleados.append(empleado)
+            return True, f"Empleado {empleado.Nombre} agregado al departamento {self._Nombre}"
+        return False, "El empleado ya está en este departamento"
+    
+    def remover_empleado(self, empleado):
+        if empleado in self._empleados:
+            self._empleados.remove(empleado)
+            return True, f"Empleado {empleado.Nombre} removido del departamento {self._Nombre}"
+        return False, "El empleado no está en este departamento"
+    
+    def listar_empleados(self):
+        return self._empleados
